bafd0d68cf565d0d2c9e871e118631edcc61e325
ashishguptabns <ashishguptabns@gmail.com>
2024-04-02 15:58:57 +0530

    working

--- src/App.js
+++ src/App.js
@@ -1,24 +1,54 @@
-import logo from './logo.svg';
 import './App.css';
 
+import { useEffect, useState, useRef } from 'react'
+import { Container, Card, Desc, Img, Title, Price, SearchInput, Products, SearchBox, Loading } from './styles';
+
 function App() {
+  const [products, setProducts] = useState([])
+  const [loading, setLoading] = useState(false)
+
+  useEffect(() => {
+    fetch('https://dummyjson.com/products')
+      .then(res => res.json())
+      .then(data => {
+        setProducts(prev => data.products)
+      });
+  }, [])
+
+  const searchTimeout = useRef(null)
+
+  const handleSearch = (event) => {
+    const term = event.target.value
+    if (searchTimeout.current) {
+      clearTimeout(searchTimeout.current)
+    }
+    setLoading(true)
+    searchTimeout.current = setTimeout(() => {
+      fetch(`https://dummyjson.com/products/search?q=${term}`)
+        .then(res => res.json())
+        .then(data => {
+          setLoading(false)
+          setProducts(prev => data.products)
+        });
+    }, 2000);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <SearchBox>
+        <SearchInput placeholder='Search...' onChange={handleSearch} />
+        <Loading>{loading && <p>Loading...</p>}</Loading>
+      </SearchBox>
+      <Products>
+        {products.length > 0 && products.map(product => <Card key={product.id}>
+          <Img src={product.thumbnail} />
+          <Title>{product.title}</Title>
+          <Desc $numLines={3}>{product.description}</Desc>
+          <Price>{product.price} INR</Price>
+          <Desc>{product.category}</Desc>
+        </Card>)}
+      </Products>
+    </Container>
   );
 }
 
